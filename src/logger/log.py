544957654593c404c370d143a428fe686181aa7b
Charbel199 <charbel-boumaroun@outlook.com>
2022-08-30 19:52:33 +0300

    Add file handler logs

--- src/logger/log.py
+++ src/logger/log.py
@@ -4,10 +4,15 @@ import logging
 def setup_custom_logger():
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
 
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
+    console_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler(f"assets/logs/simulatorGAN.log")
+
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
 
     logger = logging.getLogger('SimulatorGAN')
     logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
     return logger
