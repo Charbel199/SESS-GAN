5fa14a8877e56c5f9cde03c153ff82ac01300f72
Charbel199 <charbel-boumaroun@outlook.com>
2022-08-31 14:26:09 +0300

    Add env file for logging

--- src/logger/log.py
+++ src/logger/log.py
@@ -1,11 +1,15 @@
 import logging
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def setup_custom_logger():
     formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
 
     console_handler = logging.StreamHandler()
-    file_handler = logging.FileHandler(f"assets/logs/simulatorGAN.log")
+    file_handler = logging.FileHandler(str(os.getenv('LOG_FILE_PATH')))
 
     console_handler.setFormatter(formatter)
     file_handler.setFormatter(formatter)
