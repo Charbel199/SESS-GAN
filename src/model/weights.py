25f72c8ab98db90887382df79e1cab2b1bdd1b2f
Charbel199 <charbel-boumaroun@outlook.com>
2022-09-10 08:11:00 +0100

    Add conv weights init

--- src/model/weights.py
+++ src/model/weights.py
@@ -1,8 +1,18 @@
 import torch.nn as nn
 
 
-def initialize_model_weights(model):
+def basic_weights_init(model):
     # Initializing weights based on DCGAN paper
     for m in model.modules():
         if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
             nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+
+def conv_weights_init(model):
+    # Initializing weights based on DCGAN paper
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+        if isinstance(m, (nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            nn.init.constant_(m.bias.data, 0)
