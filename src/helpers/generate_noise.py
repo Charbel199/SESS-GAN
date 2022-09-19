b43696a7b5e7171a210f9f7fedbddfb038b23a17
Charbel199 <charbel-boumaroun@outlook.com>
2022-09-19 14:21:10 +0300

    Add singan helpers

---
+++ src/helpers/generate_noise.py
@@ -0,0 +1,9 @@
+# Code based on https://github.com/tamarott/SinGAN
+import torch
+
+
+def generate_spatial_noise(size, device):
+    """ Generates a noise tensor. """
+
+    return torch.randn(size, device=device)
+
