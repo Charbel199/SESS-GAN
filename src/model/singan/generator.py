a4b2ee62b17142c759cdcd6543374a1bfaf73789
Charbel199 <charbel-boumaroun@outlook.com>
2022-09-19 15:53:19 +0300

    Fix singan generation and discriminator

--- src/model/singan/generator.py
+++ src/model/singan/generator.py
@@ -2,26 +2,28 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from model.wgan_gp.conv_block import ConvBlock
+from model.singan.conv_block import ConvBlock
+from conf import ModelConfig
 
 
 class Generator(nn.Module):
     """ Patch based Generator. Uses namespace opt. """
-    def __init__(self, opt):
+
+    def __init__(self, config: ModelConfig):
         super().__init__()
         self.is_cuda = torch.cuda.is_available()
-        N = int(opt.nfc)
-        self.head = ConvBlock(opt.nc_current, N, (opt.ker_size, opt.ker_size), 0, 1)  # Padding is done externally
+        N = int(config.number_of_filters)
+        self.head = ConvBlock(len(config.token_list), N, (config.kernel_size, config.kernel_size), 0, 1)  # Padding is done externally
         self.body = nn.Sequential()
 
-        for i in range(opt.num_layer - 2):
-            block = ConvBlock(N, N, (opt.ker_size, opt.ker_size), 0, 1)
+        for i in range(config.number_of_layers - 2):
+            block = ConvBlock(N, N, (config.kernel_size, config.kernel_size), 0, 1)
             self.body.add_module("block%d" % (i + 1), block)
 
-        block = ConvBlock(N, N, (opt.ker_size, opt.ker_size), 0, 1)
-        self.body.add_module("block%d" % (opt.num_layer - 2), block)
+        block = ConvBlock(N, N, (config.kernel_size, config.kernel_size), 0, 1)
+        self.body.add_module("block%d" % (config.number_of_layers - 2), block)
 
-        self.tail = nn.Sequential(nn.Conv2d(N, opt.nc_current, kernel_size=(opt.ker_size, opt.ker_size),
+        self.tail = nn.Sequential(nn.Conv2d(N, len(config.token_list), kernel_size=(config.kernel_size, config.kernel_size),
                                             stride=1, padding=0))
 
     def forward(self, x, y, temperature=1):
@@ -32,4 +34,3 @@ class Generator(nn.Module):
         ind = int((y.shape[2] - x.shape[2]) / 2)
         y = y[:, :, ind:(y.shape[2] - ind), ind:(y.shape[3] - ind)]
         return x + y
-
