2a74c0b2e6096a0e5c7a2814b31af25fa5cbc062
Charbel199 <charbel-boumaroun@outlook.com>
2022-09-23 10:06:13 +0300

    Add environment loader methods

--- src/helpers/environment.py
+++ src/helpers/environment.py
@@ -3,6 +3,7 @@ from utils import load_np
 import glob
 import os
 from typing import List
+import torch.nn.functional as F
 
 
 def load_environments(environments_path, tokens, data_format) -> List:
@@ -18,5 +19,16 @@ def load_environments(environments_path, tokens, data_format) -> List:
 def tokens_to_one_hot_environment(level, tokens):
     """ Converts an environment of token to a full one-hot token tensor. """
     environment = torch.zeros((len(tokens), len(level), len(level[-1])))
-    # TODO: Add logic
+    for i in range(len(level)):
+        for j in range(len(level[-1])):
+            token = str(int(level[i][j]))
+            if token in tokens and token != "\n":
+                environment[tokens.index(token), i, j] = 1
+
+    return environment
+
+
+def one_hot_environment_to_tokens(level):
+    """ Converts a full token level tensor to an ascii level. """
+    environment = torch.argmax(level, dim=0)
     return environment
