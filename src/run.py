9a7081d6da8575da2188735c20b742c4f2dfb2f7
Charbel199 <charbel-boumaroun@outlook.com>
2022-09-25 13:15:42 +0300

    Update run

--- src/run.py
+++ src/run.py
@@ -1,18 +1,21 @@
 from conf import parse_args
 from train.cascaded_train import CascadedModelTrainer
 from logger.log import LoggerService
-from generate_samples import generate_samples
-
+from evaluate.generate_samples import generate_samples
+from evaluate.evaluate_cascaded import evaluate_cascaded
 logger = LoggerService.get_instance()
 
 
 def main():
     config = parse_args()
-    model_trainer = CascadedModelTrainer()
-    generators, noise_maps, reals, noise_amplitudes = model_trainer.train_model(config=config)
-    logger.info('Done training')
-    generate_samples(generators, noise_maps, reals,
-                     noise_amplitudes, config, input_image=None)
+    if config.save:
+        model_trainer = CascadedModelTrainer()
+        generators, noise_maps, reals, noise_amplitudes = model_trainer.train_model(config=config)
+        logger.info('Done training')
+        generate_samples(generators, noise_maps, reals,
+                         noise_amplitudes, config, input_image=None)
+    if config.load:
+        evaluate_cascaded(config)
 
 
 if __name__ == '__main__':
